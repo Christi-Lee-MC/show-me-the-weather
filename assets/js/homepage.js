d9a0681459305a292c44c79493cd24b43914acc1
Christi-Lee <christileemc@gmail.com>
2021-09-01 13:36:14 -0500

    added openweather api with my key

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
 // create api connections here
+var getCityWeather = function() {
+    fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid={af0f0e9374c73e2c02f46aba2b7be010}").then(function(response) {
+  console.log("inside", response);
+});
 
+console.log("outside");
+}
 // these are variables for the form elements
 var userFormEl = document.querySelector("#city-form");
 var nameInputEl = document.querySelector("#cityname");
